2175a18c7020cd51ca03d8e73fb81cca80a43e39
Edgar <eduardoguzmanmacedo@gmail.com>
2023-09-29 13:20:02 -0500

    done

---
+++ script.js
@@ -0,0 +1,14 @@
+let menuVisible = false;
+//Mostrar o ocultar menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+    }else{
+        document.getElementById("nav").classList = "responsive";
+    menuVisible = true;
+    }
+}
+function seleccionar(){
+    //Ocultar menu al seleccionar opcion
+}
